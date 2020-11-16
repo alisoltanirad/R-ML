e2fc0c52fd546f6abbc0cebb9ebd68a8e4e90f76
alisoltanirad <soltaniradali@gmail.com>
2020-11-16 17:27:18 +0330

    Add classifier

--- PurchaseClassification/purchase_classifier.r
+++ PurchaseClassification/purchase_classifier.r
@@ -1,13 +1,31 @@
 # https://github.com/alisoltanirad/R-ML.git
 # Classification method: Random Forest
 library(caTools)
+library(randomForest)
 
 main <- function(){
-    #
+    data = preprocess_data(get_data())
+    predictions = classify(data)
+    evaluate_classifier(data_set, predictions)
+}
+
+classify <- function(data_set){
+    print(data_set['train'][-1])
+    classifier = randomForest(x=data_set['train'][-3],
+                              y=data_set['train'][-1],
+                              ntree=500)
+    print('flag')
+    predictions = predict(classifier, newdata=data_set['test'][-3])
+    return(predictions)
+}
+
+evaluate_classifier <- function(data_set, predictions){
+    cm = table(data_set['test'][3], predictions)
+    print(cm)
 }
 
 preprocess_data <- function(data){
-    data_set$Purchased = factor(data$Purchased, levels=c(0, 1))
+    data$Purchased = factor(data$Purchased, levels=c(0, 1))
     
     split = sample.split(data$Purchased, SplitRatio=0.75)
     train_set = subset(data, split==TRUE)
