b1aa92ac8cbf250e9d7d1ed81b1718ff153c94dc
alisoltanirad <soltaniradali@gmail.com>
2020-11-21 17:33:17 +0330

    Add preprocess data function

--- ProfitRegression/profit_regressor.r
+++ ProfitRegression/profit_regressor.r
@@ -3,6 +3,21 @@
 library(caTools)
 library(e1071)
 
+preprocess_data <- function(data){
+    data$State = factor(data$State,
+                        levels=c('New York', 'California', 'Florida'),
+                        labels=c(1, 2, 3))
+    
+    split = sample.split(data$Profit, SplitRatio=0.8)
+    train_set = subset(data, split==TRUE)
+    test_set = subset(data, split==FALSE)
+    
+    data_set = list(train_set, test_set)
+    names(data_set) = c('train', 'test')
+    
+    return(data_set)
+}
+
 get_data <- function(){
     return(read.csv('Sturtups.csv')[3:5])
 }
